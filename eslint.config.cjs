595b2f07fbaeeb9c622f162cfe888d48f71a8c97
Vlad <1986mbrovski@gmail.com>
2025-11-16 17:18:40 +0200

    chore: stabilize public API; forbid any; add package exports

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,9 +1,9 @@
 const js = require('@eslint/js');
 const tseslint = require('typescript-eslint');
 const prettier = require('eslint-config-prettier');
-
+const globals = require('globals');
 module.exports = [
-  { ignores: ['**/*.cjs'] }, // ігноруємо конфіги CJS
+  { ignores: ['**/*.cjs', 'dist', 'node_modules'] }, // ігноруємо конфіги CJS
   js.configs.recommended, // базові правила JS
   ...tseslint.configs.recommended, // базові правила TS
   prettier, // вимикає правила, що конфліктують із Prettier
@@ -15,12 +15,13 @@ module.exports = [
         ecmaVersion: 'latest',
         sourceType: 'module',
       },
+      globals: { ...globals.node },
     },
     plugins: {
       '@typescript-eslint': tseslint.plugin,
     },
     rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-explicit-any': 'error',
       'no-unused-vars': 'warn',
     },
   },
